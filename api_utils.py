9cfb76571db491bdc43f2e5fe96d16ff866dcff2
Erik Arakelyan <erikarakelyan1997@gmail.com>
2018-04-17 21:07:31 +0400

    added download option for pretrained fastText

--- api_utils.py
+++ api_utils.py
@@ -5,6 +5,10 @@ import re
 import json
 import time
 import argparse
+import requests
+import urllib.request
+import shutil
+
 from fast_utils import exact_word_match
 
 
@@ -44,6 +48,57 @@ def spacy_model_download(model_name, timeout = None):
     except (DownloadError, Exception) as e:
         print(e)
 
+
+def fasttext_list():
+
+    diction_frac = {}
+    try:
+        content = requests.get("https://github.com/facebookresearch/fastText/blob/master/pretrained-vectors.md").content
+        table = LH.fromstring(content)
+
+        webpage = LH.fromstring(content)
+        allRefs = webpage.xpath('//a/@href')
+
+        allRefs = [i for i in allRefs if 'amazonaws' in i and not 'zip' in i]
+        allRefs
+
+        df = pd.read_html(content)
+        df = df[-1]
+
+        assert(len(allRefs) ==  len(df['Unnamed: 0']) + len(df['Unnamed: 1'])+len(df['Unnamed: 2']))
+
+        for i in range(len(allRefs)):
+            if i%3 == 0:
+                diction_frac[df['Unnamed: 0'][int(i/3)]] = allRefs[i]
+            if i%3 == 1:
+                diction_frac[df['Unnamed: 1'][int(i/3)]] = allRefs[i]
+            if i%3 == 2:
+                diction_frac[df['Unnamed: 2'][int(i/3)]] = allRefs[i]
+
+
+    except Exception as e:
+        print(e)
+        return None
+
+    return diction_frac
+
+def fastext_dowload(language_name, timeout = None):
+    try:
+        full_lang_dict = fasttext_list()
+        url = ''
+        for key in full_lang_dict:
+            if language_name.lower() in key.lower():
+                url = full_lang_dict[key]
+                file_name = url.split('/')[-1]
+                
+        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+
+    except e as Exception:
+        print(e)
+
+
+
 def python_exec():
     if sys.version_info <(3,):
         return 'python'
@@ -54,12 +109,14 @@ def python_exec():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Example with long option names')
-    parser.add_argument('-l', action="store", dest = 'location', help = 'Location of the model, i.e gensim, spacy etc etc')
+    parser.add_argument('-l', action="store", dest = 'location', help = 'Location of the model, i.e gensim, spacy, fastText etc etc')
     parser.add_argument('-m', action="store", type=str, dest = 'model_name', help ='designated model name')
     parser.add_argument('-t', action="store", type=str, dest = 'timeout', help ='timeout', default = None)
 
     results = parser.parse_args()
     print(results)
 
-    if 'spacy' in results.location:
+    if 'spacy' in results.location.lower():
         spacy_model_download(results.model_name, results.timeout)
+    if 'fasttext' in results.location.lower():
+        fastext_dowload(results.model_name, results.timeout)
